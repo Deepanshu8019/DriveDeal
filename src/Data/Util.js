f8cdf65242c420db6c8c5fb712d8b56420018212
Deepanshu8019 <deepanshushuka2723@gmail.com>
2025-01-04 23:58:53 +0530

    first commit

--- src/Data/Util.js
+++ src/Data/Util.js
@@ -1,24 +1,30 @@
 export const removeImage = async (url) => {
-    try {
-          const publicId = extractPublicId(url);
-          const BaseUrl = import.meta.env.VITE_APP_API_URL
-          const res = await fetch(`${BaseUrl}/removeImage`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            public_id: publicId,
-          }),
-        });
-  
-        const response = await res.json();
-        return response;
-    } catch (error) {
-        console.error("Error removing image:", error);
-        return {};
+  try {
+    const publicId = extractPublicId(url);
+    const BaseUrl = import.meta.env.VITE_APP_API_URL;
+    
+    const res = await fetch(`${BaseUrl}/removeImage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        public_id: publicId,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Failed to remove image: ${res.statusText}`);
     }
-  };
+
+    const response = await res.json();
+    return response;
+  } catch (error) {
+    console.error("Error removing image:", error);
+    return { success: false, message: error.message };
+  }
+};
+
 
 export const extractPublicId = (url) => {
     const parts = url.split('/');
